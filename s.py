3d54929a8e68ea6f28bebd8947861df8a464d9d0
justin <justintan28@gmail.com>
2022-08-29 17:43:07 +0800

    fixed all variables to camel case; all functions to snake case

--- s.py
+++ s.py
@@ -2,21 +2,21 @@
 # Design Doc - https://docs.google.com/document/d/1KCCfjfKg8iyMkaLdQSvfWO71qLnZUjACPIxyBmeREjo/edit?usp=sharing
 
 # Functions
-def print_matrix(input_matrix):
+def print_matrix(inputMatrix):
     for x in range(9):
-        first_index_in_row = 0 + (9 * x)
-        row_of_numbers = input_matrix[first_index_in_row:first_index_in_row+9]
-        formatted_list = [f'{y["val"]:^3}' for y in row_of_numbers]
-        spaced_string = "|".join(formatted_list)
-        print(spaced_string)
+        firstIndexInRow = 0 + (9 * x)
+        rowOfNumbers = inputMatrix[firstIndexInRow:firstIndexInRow+9]
+        formattedList = [f'{y["val"]:^3}' for y in rowOfNumbers]
+        spacedString = "|".join(formattedList)
+        print(spacedString)
         if x < 8:
-            print("-" * len(spaced_string))
+            print("-" * len(spacedString))
         
-def mat(row,column):
-    index_within_row = column - 1
-    elem_from_rows_above = 9 * (row - 1)
-    final_index = elem_from_rows_above + index_within_row
-    return final_index
+def master_index(row,column):
+    indexWithinRow = column - 1
+    elemFromRowsAbove = 9 * (row - 1)
+    finalIndex = elemFromRowsAbove + indexWithinRow
+    return finalIndex
 
 def debug_matrix(option):
     print_matrix(master)
@@ -27,95 +27,75 @@ def debug_matrix(option):
             print()
 
 # Example String
-sample_string = "004300209005009001070060043006002087190007400"\
+sampleString = "004300209005009001070060043006002087190007400"\
                 "050083000600000105003508690042910300"
 
 # 9block Mechanism
-""" master_9block = {
-                 "row":{
-                        "cells" :{str(x):[] for x in range (1,10)},
-                        "sols"  :{str(x):[] for x in range (1,10)},
-                        "spaces":{str(x):0 for x in range (1,10)}
-                        }, 
-                 "col":{
-                        "cells" :{str(x):[] for x in range (1,10)},
-                        "sols"  :{str(x):[] for x in range (1,10)},
-                        "spaces":{str(x):0 for x in range (1,10)}
-                        },
-                 "mtx":{
-                        "cells" :{str(x):[] for x in range (1,10)},
-                        "sols"  :{str(x):[] for x in range (1,10)},
-                        "spaces":{str(x):0 for x in range (1,10)}
-                        },
-                 } """
-master_9block = {
+master9block = {
                  "row":{str(x):{"cells":[],"rows":[],"spaces":0} for x in \
-                            range (1,10)
-                        }, 
+                            range (1,10)}, 
                  "col":{str(x):{"cells":[],"rows":[],"spaces":0} for x in \
-                            range (1,10)
-                        },
+                            range (1,10)},
                  "mtx":{str(x):{"cells":[],"rows":[],"spaces":0} for x in \
-                            range (1,10)
-                        },
+                            range (1,10)},
                  }
 
 # Cell Mechanism
-master = [{"val":"", "sol":[], "row_no":"", "col_no":"", "mtx_no":""} for x in 
+master = [{"val":"", "sol":[], "rowNo":"", "colNo":"", "mtxNo":""} for x in 
           range(81)]
 for idx,cell in enumerate(master):
     
     # set individual cell value
-    cell["val"] = sample_string[idx]
+    cell["val"] = sampleString[idx]
     
     # row
-    row_no = str((idx // 9) + 1)
-    cell["row_no"] = row_no
+    rowNo = str((idx // 9) + 1)
+    cell["rowNo"] = rowNo
     
     # col    
-    col_no = str((idx % 9) + 1)
-    cell["col_no"] = col_no
+    colNo = str((idx % 9) + 1)
+    cell["colNo"] = colNo
     
     # mtx
-    if cell["row_no"] in ("1","2","3"):
-        if cell["col_no"] in ("1","2","3"):
-            mtx_no = "1"
-        elif cell["col_no"] in ("4","5","6"):
-            mtx_no = "2"
-        elif cell["col_no"] in ("7","8","9"):
-            mtx_no = "3"
-    elif cell["row_no"] in ("4","5","6"):
-        if cell["col_no"] in ("1","2","3"):
-            mtx_no = "4"
-        elif cell["col_no"] in ("4","5","6"):
-            mtx_no = "5"
-        elif cell["col_no"] in ("7","8","9"):
-            mtx_no = "6"
-    elif cell["row_no"] in ("7","8","9"):
-        if cell["col_no"] in ("1","2","3"):
-            mtx_no = "7"
-        elif cell["col_no"] in ("4","5","6"):
-            mtx_no = "8"
-        elif cell["col_no"] in ("7","8","9"):
-            mtx_no = "9"
-    cell["mtx_no"] = mtx_no
+    if cell["rowNo"] in ("1","2","3"):
+        if cell["colNo"] in ("1","2","3"):
+            mtxNo = "1"
+        elif cell["colNo"] in ("4","5","6"):
+            mtxNo = "2"
+        elif cell["colNo"] in ("7","8","9"):
+            mtxNo = "3"
+    elif cell["rowNo"] in ("4","5","6"):
+        if cell["colNo"] in ("1","2","3"):
+            mtxNo = "4"
+        elif cell["colNo"] in ("4","5","6"):
+            mtxNo = "5"
+        elif cell["colNo"] in ("7","8","9"):
+            mtxNo = "6"
+    elif cell["rowNo"] in ("7","8","9"):
+        if cell["colNo"] in ("1","2","3"):
+            mtxNo = "7"
+        elif cell["colNo"] in ("4","5","6"):
+            mtxNo = "8"
+        elif cell["colNo"] in ("7","8","9"):
+            mtxNo = "9"
+    cell["mtxNo"] = mtxNo
     
-    # 9block
-    master_9block["row"][row_no]["cells"].append(cell)
-    master_9block["col"][col_no]["cells"].append(cell)
-    master_9block["mtx"][mtx_no]["cells"].append(cell)
+    # add cell references to 9blocks
+    master9block["row"][rowNo]["cells"].append(cell)
+    master9block["col"][colNo]["cells"].append(cell)
+    master9block["mtx"][mtxNo]["cells"].append(cell)
 
 # Playground
 debug_matrix(False)
 print()
 while True:                                                     # 9block checker
-    inp_9b = input("What 9block do you want? >")
-    if inp_9b == "done":break
+    inp9b = input("What 9block do you want? >")
+    if inp9b == "done":break
     while True:
-        inp_no = input(f"Which {inp_9b} do you want? >")
-        if inp_no == "back":break
-        if (int(inp_no)<1) or (int(inp_no)>9):continue
-        for thing in master_9block[inp_9b][inp_no]["cells"]:print(thing)
+        inpNo = input(f"Which {inp9b} do you want? >")
+        if inpNo == "back":break
+        if (int(inpNo)<1) or (int(inpNo)>9):continue
+        for thing in master9block[inp9b][inpNo]["cells"]:print(thing)
         print()
         debug_matrix(False)
         print()
