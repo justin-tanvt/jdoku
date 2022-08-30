936525d72d4877586f9f6f5489cc32e0b60c50c7
justin <justintan28@gmail.com>
2022-08-30 16:28:52 +0800

    checkpoint: algo1 acc but incomplete; algo2 unsure; mistake when combined; ans checker next

--- s.py
+++ s.py
@@ -9,7 +9,10 @@ def print_matrix(inputMatrix):
     for x in range(9):
         firstIndexInRow = 0 + (9 * x)
         rowOf9Numbers = inputMatrix[firstIndexInRow:firstIndexInRow+9]
-        centredNumbers = [f'{y["val"]:^3}' for y in rowOf9Numbers]
+        if isinstance(inputMatrix,list):
+            centredNumbers = [f'{y["val"]:^3}' for y in rowOf9Numbers]
+        elif isinstance(inputMatrix,str):
+            centredNumbers = [f'{y:^3}' for y in rowOf9Numbers]
         boxedCentredNumbers = "|".join(centredNumbers)
         print(boxedCentredNumbers)
         if x < 8:
@@ -32,49 +35,47 @@ def overall_solve(quesString,ansString):
     # Cell Mechanism
     master = [{"val":"", "sol":[], "rowNo":"", "colNo":"", "mtxNo":""}
             for x in range(81)]
-    for idx,cell in enumerate(master):
+    for idx,masterCell in enumerate(master):
         
         # set individual cell value
-        cell["val"] = quesString[idx]
+        masterCell["val"] = quesString[idx]
         
         # row
         rowNo = str((idx // 9) + 1)
-        cell["rowNo"] = rowNo
+        masterCell["rowNo"] = rowNo
         
         # col    
         colNo = str((idx % 9) + 1)
-        cell["colNo"] = colNo
+        masterCell["colNo"] = colNo
         
         # mtx
-        if cell["rowNo"] in ("1","2","3"):
-            if cell["colNo"] in ("1","2","3"):
+        if masterCell["rowNo"] in ("1","2","3"):
+            if masterCell["colNo"] in ("1","2","3"):
                 mtxNo = "1"
-            elif cell["colNo"] in ("4","5","6"):
+            elif masterCell["colNo"] in ("4","5","6"):
                 mtxNo = "2"
-            elif cell["colNo"] in ("7","8","9"):
+            elif masterCell["colNo"] in ("7","8","9"):
                 mtxNo = "3"
-        elif cell["rowNo"] in ("4","5","6"):
-            if cell["colNo"] in ("1","2","3"):
+        elif masterCell["rowNo"] in ("4","5","6"):
+            if masterCell["colNo"] in ("1","2","3"):
                 mtxNo = "4"
-            elif cell["colNo"] in ("4","5","6"):
+            elif masterCell["colNo"] in ("4","5","6"):
                 mtxNo = "5"
-            elif cell["colNo"] in ("7","8","9"):
+            elif masterCell["colNo"] in ("7","8","9"):
                 mtxNo = "6"
-        elif cell["rowNo"] in ("7","8","9"):
-            if cell["colNo"] in ("1","2","3"):
+        elif masterCell["rowNo"] in ("7","8","9"):
+            if masterCell["colNo"] in ("1","2","3"):
                 mtxNo = "7"
-            elif cell["colNo"] in ("4","5","6"):
+            elif masterCell["colNo"] in ("4","5","6"):
                 mtxNo = "8"
-            elif cell["colNo"] in ("7","8","9"):
+            elif masterCell["colNo"] in ("7","8","9"):
                 mtxNo = "9"
-        cell["mtxNo"] = mtxNo
+        masterCell["mtxNo"] = mtxNo
         
         # add cell references to 9blocks
-        master9block["rows"][rowNo]["cells"].append(cell)
-        master9block["cols"][colNo]["cells"].append(cell)
-        master9block["mtxs"][mtxNo]["cells"].append(cell)
-
-    print_matrix(master)
+        master9block["rows"][rowNo]["cells"].append(masterCell)
+        master9block["cols"][colNo]["cells"].append(masterCell)
+        master9block["mtxs"][mtxNo]["cells"].append(masterCell)
     
     # Solution-generator Mechanism
     numberList = [str(x) for x in range(1,10)]
@@ -107,50 +108,55 @@ def overall_solve(quesString,ansString):
                 print(rcmType,currentRCM,a,[(elem["rowNo"],elem["colNo"])
                                             for elem in b]) """
 
-    def solution_algorithm_1(cell):
+    def solution_algorithm_1(cellSA1):
         # implement solution
-        if len(cell["sol"]) != 1:return
-        currentSolution = cell["sol"][0]
-        cell["val"] = currentSolution
-        """ print(f"Solution found: (R,C={cell['rowNo']},{cell['colNo']}) "
-            f"for value ({currentSolution})\n")
-        print_matrix() """
+        if len(cellSA1["sol"]) != 1:return
+        currentSolution = cellSA1["sol"][0]
+        cellSA1["val"] = currentSolution
+        print(f"Solution found: (R,C={cellSA1['rowNo']},{cellSA1['colNo']}) "
+            f"for value ({currentSolution})")
+        print_matrix(master)
         # update solutions in 9block cells
-        cell["sol"] = []
+        cellSA1["sol"] = []
         for rcmType,rcmNo in rcmTuple:
-            for otherCell in master9block[rcmType][cell[rcmNo]]["cells"]:
+            for otherCell in master9block[rcmType][cellSA1[rcmNo]]["cells"]:
                 try:otherCell["sol"].remove(currentSolution)
                 except:pass
-            master9block[rcmType][cell[rcmNo]]["sols"][currentSolution] = []
-            print(f"emptied out {rcmType}{cell[rcmNo]}'s ({currentSolution}) solution locations.")
+            master9block[rcmType][cellSA1[rcmNo]]["sols"][currentSolution] = []
+            # print(f"emptied out {rcmType}{cellSA1[rcmNo]}'s ({currentSolution}) solution locations.")
         
-    # def solution_algorithm_2(solutions,sol):
-    #     if len(solutions[sol]) != 1:return
-    #     solutions[sol][0]["val"] = sol
-    #     solutions[sol] = []
-    #     # remove solution locations from similar solutions within RCM
+    # def solution_algorithm_2(solSA2,solutionsSA2):
+    #     if len(solutionsSA2[solSA2]) != 1:return
+    #     cellSA2 = solutionsSA2[solSA2][0]
+    #     cellSA2["val"] = solSA2
+    #     print(f"Solution found: (R,C={cellSA2['rowNo']},{cellSA2['colNo']}) "
+    #         f"for value ({solSA2})\n")
+    #     print_matrix(master)
+    #     solutionsSA2[solSA2] = []
     #     for rcmType,rcmNo in rcmTuple:
-    #         for otherCell in master9block[rcmType][cell[rcmNo]]["cells"]:
-    #             try:otherCell["sol"].remove(currentSolution)
+    #         for otherCell in master9block[rcmType][cellSA2[rcmNo]]["cells"]:
+    #             try:otherCell["sol"].remove(solSA2)
     #             except:pass
+    #         master9block[rcmType][cellSA2[rcmNo]]["sols"][solSA2] = []
         
 
     # Iteration Mechanism
-    # while True:
     while True:
         for rcmType,rcmNo in rcmTuple:
             for currentRCM in master9block[rcmType]:
                 cellsInCurrentRCM = master9block[rcmType][currentRCM]["cells"]
-                solsInCurrentRCM = master9block[rcmType][currentRCM]["sols"]
                 for currentCell in cellsInCurrentRCM:
                     solution_algorithm_1(currentCell)
-                # for currentSol in solsInCurrentRCM:
-                #     solution_algorithm_2(solsInCurrentRCM,currentSol)
+        # for rcmType,rcmNo in rcmTuple:
+        #     for currentRCM in master9block[rcmType]:
+        #         solsInCurrentRCM = master9block[rcmType][currentRCM]["sols"]
+        #         for currentSol in solsInCurrentRCM:
+        #             solution_algorithm_2(currentSol,solsInCurrentRCM)
         finalAnswer = ''.join([elem["val"] for elem in master])
         if finalAnswer == ansString:
-            """ currentSolveCount += 1
-            print(f"Sudoku #{currentSolveCount} of {totalSudokuCount} "\
-                "has been solved.") """
+            # currentSolveCount += 1
+            # print(f"Sudoku #{currentSolveCount} of {totalSudokuCount} "\
+            #     "has been solved.")
             print("Sudoku solved...")
             break
         else:continue
@@ -176,10 +182,13 @@ sample1 = "500104090006030002091070003070000060650001904930408500240605087800302
 sample2 = "068700900004000071030809050300080100040005007007304092602001005000020600059030028","568712943924653871731849256395287164246195387817364592682971435473528619159436728"
 overall_solve(sample1[0],sample1[1])
 
+# Print Answer
+# print_matrix(sample2[1])
+
 # Proper - Run Program
 """ totalSudokuCount = 0
 currentSolveCount = 0
-filename = "sudoku_small_3611.csv"
+filename = "sudoku_small.csv"
 file = open(filename)
 csvreader = csv.reader(file)
 for line in csvreader:
